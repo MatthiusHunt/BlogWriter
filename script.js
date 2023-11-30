3b221675150b6cd0a21a4e4214e8a4343e265227
MatthiusHunt <152246347+MatthiusHunt@users.noreply.github.com>
2023-11-29 20:21:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
-function generateBigIdeas() {
-    // Get the intro content
-    const title = document.getElementById('titleContent').value;
-    const intro = document.getElementById('introContent').value;
+let currentSectionIndex = 0;
+const sections = [
+    'titleContent',
+    'introContent',
+    'bigIdea1Content',
+    'bigIdea2Content',
+    'bigIdea3Content',
+    'conclusionContent',
+    'callToAction'
+];
 
-    // Clear the bigIdeasContainer
-    document.getElementById('bigIdeasContainer').innerHTML = '';
+function goToNext(nextSection) {
+    if (currentSectionIndex < sections.length - 1) {
+        document.getElementById(sections[currentSectionIndex]).style.display = 'none';
+        currentSectionIndex++;
+        document.getElementById(sections[currentSectionIndex]).style.display = 'block';
 
-    // Create a Big Idea block for each of the three ideas
-    ['VariableA', 'VariableB', 'VariableC'].forEach((variable, index) => {
-        const bigIdeaBlock = document.createElement('div');
-        bigIdeaBlock.className = 'block';
-
-        // Create header for the Big Idea block
-        const header = document.createElement('h2');
-        header.innerText = `${title} - Big Idea ${index + 1}: (${variable})`;
-        bigIdeaBlock.appendChild(header);
-
-        // Create input fields for each argument
-        for (let j = 1; j <= 3; j++) {
-            const argumentLabel = document.createElement('label');
-            argumentLabel.innerText = `Argument ${j}:`;
-
-            const argumentInput = document.createElement('input');
-            argumentInput.type = 'text';
-
-            bigIdeaBlock.appendChild(argumentLabel);
-            bigIdeaBlock.appendChild(argumentInput);
+        if (currentSectionIndex === 3 || currentSectionIndex === 4) {
+            // Update the header for the Big Idea sections
+            const bigIdeaHeaderText = document.getElementById(sections[currentSectionIndex - 1]).value;
+            document.getElementById(`bigIdea${currentSectionIndex - 2}Header`).innerText = bigIdeaHeaderText;
         }
+    }
+}
 
-        // Create input field for the Value Shot
-        const valueShotLabel = document.createElement('label');
-        valueShotLabel.innerText = 'Value Shot:';
-
-        const valueShotInput = document.createElement('input');
-        valueShotInput.type = 'text';
-
-        bigIdeaBlock.appendChild(valueShotLabel);
-        bigIdeaBlock.appendChild(valueShotInput);
-
-        // Append the Big Idea block to the container
-        document.getElementById('bigIdeasContainer').appendChild(bigIdeaBlock);
-    });
+function finish() {
+    // Display the final content
+    const finalContent = sections.map(section => {
+        return `${document.getElementById(section).value}`;
+    }).join('\n\n');
+    
+    alert(finalContent);
 }
