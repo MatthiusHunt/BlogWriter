4489ef14bda069170d1d40c3099726ce73f37062
MatthiusHunt <152246347+MatthiusHunt@users.noreply.github.com>
2023-11-29 20:06:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function generateBigIdeas() {
     // Get the intro content
-    const introTitle = document.getElementById('introTitle').value;
-    const introContent = document.getElementById('introContent').value;
+    const title = document.getElementById('titleContent').value;
+    const intro = document.getElementById('introContent').value;
 
     // Clear the bigIdeasContainer
     document.getElementById('bigIdeasContainer').innerHTML = '';
 
     // Create a Big Idea block for each of the three ideas
-    for (let i = 1; i <= 3; i++) {
+    ['VariableA', 'VariableB', 'VariableC'].forEach((variable, index) => {
         const bigIdeaBlock = document.createElement('div');
         bigIdeaBlock.className = 'block';
-        
+
         // Create header for the Big Idea block
         const header = document.createElement('h2');
-        header.innerText = `${introTitle} - Big Idea ${i}`;
+        header.innerText = `${title} - Big Idea ${index + 1}: (${variable})`;
         bigIdeaBlock.appendChild(header);
-        
+
         // Create input fields for each argument
         for (let j = 1; j <= 3; j++) {
             const argumentLabel = document.createElement('label');
             argumentLabel.innerText = `Argument ${j}:`;
-            
+
             const argumentInput = document.createElement('input');
             argumentInput.type = 'text';
 
@@ -30,7 +30,7 @@ function generateBigIdeas() {
 
         // Create input field for the Value Shot
         const valueShotLabel = document.createElement('label');
-        valueShotLabel.innerText = 'Value Shot (Data, Picture/meme, video, etc):';
+        valueShotLabel.innerText = 'Value Shot:';
 
         const valueShotInput = document.createElement('input');
         valueShotInput.type = 'text';
@@ -40,6 +40,5 @@ function generateBigIdeas() {
 
         // Append the Big Idea block to the container
         document.getElementById('bigIdeasContainer').appendChild(bigIdeaBlock);
-    }
+    });
 }
-
